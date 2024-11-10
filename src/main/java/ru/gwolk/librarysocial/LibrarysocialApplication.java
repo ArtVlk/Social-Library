dccf97e1c193a51436955b7b86eb99be7b8e45ab
Artem Volk <artem_volk04@mail.ru>
2024-11-10 19:02:57 +0500

    add project

---
+++ src/main/java/ru/gwolk/librarysocial/LibrarysocialApplication.java
@@ -0,0 +1,13 @@
+package ru.gwolk.librarysocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarysocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarysocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gwolk/librarysocial/LibrarysocialApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gwolk.librarysocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarysocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
